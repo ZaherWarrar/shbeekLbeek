e55ba035d1d4b0e8970964121165a5a6e476f237
ZaherWarrar <zaherwarrar@gmail.com>
2025-11-05 17:10:01 +0300

    edit color

--- lib/core/constant/app_color.dart
+++ lib/core/constant/app_color.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColor {
   Color backgroundColor = const Color.fromARGB(255, 244, 244, 244);
   Color backgroundColorCard = Colors.white;
-  Color primaryColor = Colors.blue;
+  Color primaryColor = Colors.orange;
   Color titleColor = Colors.black;
   Color textButomColor = Colors.white;
   Color descriptionColor = Colors.blueGrey;

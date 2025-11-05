cd63e11a3d34f4c2b1f58ceffc693f19b110e54b
ZaherWarrar <zaherwarrar@gmail.com>
2025-11-05 17:08:18 +0300

    ..

--- lib/view/home_page/widget/category_type/category_type.dart
+++ lib/view/home_page/widget/category_type/category_type.dart
@@ -1,6 +1,4 @@
 import 'package:app/controller/category/category_type_controller.dart';
-import 'package:app/core/constant/app_color.dart';
-import 'package:app/core/function/fontsize.dart';
 import 'package:app/view/home_page/widget/category_type/widgets/category_items.dart';
 import 'package:app/view/home_page/widget/category_type/widgets/main_category.dart';
 import 'package:flutter/material.dart';

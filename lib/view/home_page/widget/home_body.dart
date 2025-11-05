bb525838877fbecb3d20f5bd3f549ea8fac1abc2
ZaherWarrar <zaherwarrar@gmail.com>
2025-11-05 14:07:22 +0300

    ..

--- lib/view/home_page/widget/home_body.dart
+++ lib/view/home_page/widget/home_body.dart
@@ -1,12 +1,5 @@
 import 'package:app/core/constant/app_color.dart';
-
-import 'package:app/view/home_page/widget/category_slider.dart';
-
 import 'package:app/core/shared/custom_slider.dart';
-
-
-import 'package:app/core/shared/custom_slider.dart';
-
 import 'package:app/view/home_page/widget/custom_home_app_bar.dart';
 import 'package:flutter/material.dart';
 
